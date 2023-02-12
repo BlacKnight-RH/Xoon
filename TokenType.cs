578805afa64e072a1b3441c2fa593b016fc2628e
x33r0 <mahmoud_magdy_m@outlook.com>
2023-02-12 07:18:52 +0200

    [ADD] Add basic structure for  TokenType

---
+++ TokenType.cs
@@ -0,0 +1,31 @@
+namespace Xoon.Tokens;
+
+
+internal enum TokenType
+{
+    LEFT_PAREN, RIGHT_PAREN, // ()
+    LEFT_BRACE, RIGHT_BRACE, // {}
+    LEFT_BRACKET, RIGHT_BRACKET, // []
+
+    BANG, BANG_EQUAL, // !, !=
+    EQUAL, EQUAL_EQUAL, // = , ==
+    GREATER, GREATER_EQUAL, // >, >=
+    LESS, LESS_EQUAL, // <, <=
+
+    
+    IDENTIFIER, STRING, NUMBER,
+
+    AND, OR, 
+    IF, ELSE, 
+    FOR, WHILE,
+    TRUE, FALSE, NIL,
+    FUN, RETURN,
+    THIS, SUPER,VAR,
+    CLASS,
+    PRINT,
+
+    COMMA, DOT, SEMICOLON, // , . ;
+    MINUS, PLUS, STAR, // - + * 
+
+    EOF,
+}
