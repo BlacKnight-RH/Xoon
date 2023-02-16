3eab420195db337a3c630899d121ce8d2c0d2024
x33r0 <mahmoud_magdy_m@outlook.com>
2023-02-16 23:37:35 +0200

    [ADD] Add exit Codes util enum

---
+++ Utilities/ExitStatusCode.cs
@@ -0,0 +1,8 @@
+namespace Xoon.Utilities;
+
+// https://man.freebsd.org/cgi/man.cgi?query=sysexits&apropos=0&sektion=0&manpath=FreeBSD+4.3-RELEASE&format=html
+public static class ExitStatusCode
+{
+    public const int USAGE = 64;
+    public const int DATA_ERR = 65;
+}
